fa4155f950e417d995d726f1a9602a68e2b12330
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2024-01-15 03:16:43 -0500

    done

--- RST/1/1/Full-Adder.ino
+++ RST/1/1/Full-Adder.ino
@@ -1,9 +1,10 @@
 // Created on: Jan 2024
-// Created by: Emmanuel
-// This is a full-adder with arduino
+// Created by: James Couse
+// Created for: Full-adder with arduino
 
 const int DIGIT_A1 = 13;
 const int DIGIT_B1 = 12;
+const int CARRY_IN = 7;
 
 void setup()
 {
@@ -12,26 +13,29 @@ void setup()
   // set pin mode to output
   pinMode(DIGIT_A1, OUTPUT);
   pinMode(DIGIT_B1, OUTPUT);
+  pinMode(CARRY_IN, OUTPUT);
   
   // set all pins to "0)
   digitalWrite(DIGIT_A1, 0); //LOW and HIGH just reference 0 & 1
   digitalWrite(DIGIT_B1, 0);
+  digitalWrite(CARRY_IN, 0);
   
   // loop over the inputs
   Serial.print("Starting ... \n\n");
   delay(1000); // Wait for 1000 millisecond(s)
-    for (int Cin = 0; Cin <= 1; ++Cin) {
-      digitalWrite(DIGIT_CIN, Cin);
-      for (int onesA = 0; onesA <= 1; ++onesA) {
-        digitalWrite(DIGIT_A1, onesA);
-        for (int onesB = 0; onesB <= 1; ++onesB) {
-          digitalWrite(DIGIT_B1, onesB);
 
-      Serial.print("A1 = " + String(onesA) + "\n");
-      Serial.print("B1 = " + String(onesB) + "\n");
-      Serial.print(String(onesA) + " + " + String(onesB) + " = " + String(onesA + onesB) + "\n\n");
-      delay(2000); // Wait for 2000 millisecond(s)
-      
+  for (int onesA = 0; onesA <= 1; ++onesA) {
+    digitalWrite(DIGIT_A1, onesA);
+    for (int onesB = 0; onesB <= 1; ++onesB) {
+      digitalWrite(DIGIT_B1, onesB);
+      for (int cIn = 0; cIn <= 1; ++cIn) {
+        digitalWrite(CARRY_IN, cIn);
+        Serial.print("A1 = " + String(onesA) + "\n");
+        Serial.print("B1 = " + String(onesB) + "\n");
+        Serial.print("Carry In = " + String(cIn) + "\n");
+        Serial.print(String(onesA) + " + " + String(onesB) + " + " + String(cIn) + " = " + String(onesA + onesB + cIn) + "\n\n");
+        delay(6000); // Wait for 2000 millisecond(s)
+      }
     }
   }
 }
